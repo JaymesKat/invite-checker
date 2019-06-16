8c2741c7a68ab9c4a01897f4f9027dead9fa6b4e
James Katarikawe <james.katarikawe@andela.com>
2019-06-16 16:02:14 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'World'}));
+});
+app.listen(port, function () {
+ console.log('Invite checker app listening on port !');
+});
